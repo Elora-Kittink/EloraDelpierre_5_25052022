9aebcb856ad1ef3dba063a1f05303afa3b271a18
Elora <edelpierre@exomind.fr>
2022-05-30 17:25:04 +0200

    calculation class created in model

--- CountOnMe/Model/Calculator.swift
+++ CountOnMe/Model/Calculator.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+
+class Calculation {
+    
+    func calculation(elements: [String]) -> Double {
+        var elements = elements
+        while elements.count >= 3 {
+            let left = Int(elements[0])!
+            let operand = elements[1]
+            let right = Int(elements[2])!
+            // faire l'opération des trois premiers éléments
+            let result: Int
+            switch operand {
+            case "+": result = left + right
+            case "-": result = left - right
+            default: fatalError("Unknown operator !")
+            }
+            //Pour gérer les opération à plus de trois éléments
+            // une fois que les trois premiers éléments sont traités, les retirer de la phrase
+            elements = Array(elements.dropFirst(3))
+            // et ajouter le résultat de l'opération au début du tableau
+            elements.insert("\(result)", at: 0)
+            // on reste dans la boucle tant qu'il reste des opération après les trois premières
+        }
+
+        return Double(elements[0]) ?? 0
+    }
+}
